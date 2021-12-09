4209cf43e06c7d5599da2744d110779d34a13dd0
amakaogujiofor <80483692+amakaogujiofor@users.noreply.github.com>
2021-12-09 17:24:15 +0100

    Added Logic to generate new quotes

---
+++ script.js
@@ -0,0 +1,52 @@
+// Display Quotes
+const quoteContainer = document.getElementById("quote-container");
+const authorText = document.getElementById("author");
+const quoteText = document.getElementById("quote");
+const twitterBtn = document.getElementById("twitter");
+const newQuote = document.getElementById("new-quote");
+
+// API Quotes
+let apiQuotes = [];
+
+// Show New Quotes
+function newQuotes() {
+  // Get Random Quotes
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+
+  // Replace Author field if blank
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+
+  //   Long Quote Style
+  if (quote.text.length > 50) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+
+  quoteText.textContent = quote.text;
+}
+
+async function getQuotes() {
+  const apiUrl = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuotes();
+  } catch (error) {
+    //   Catch Error Here
+    alert("Help, error");
+  }
+}
+
+// Tweet a quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterBtn, "_blank");
+}
+
+// On load
+getQuotes();
